ad73aa736ad80c8fc59288be47a7bcf6411163f4
Kyle Mitchell <kyle@kemitchell.com>
2015-12-08 15:06:08 -0800

    Print test success messages

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e # Fail on error.
 set -v # Print commands to stderr as executed.
-node test/validate-inputs.js
-node test/compare-blanks-and-inputs.js
-! cat *.cform | aspell --dont-backup list --personal=./whitelist.en.pws | grep "^"
+node test/validate-inputs.js && echo "Inputs validate"
+node test/compare-blanks-and-inputs.js && echo "Inputs match template"
+! cat *.cform | aspell --dont-backup list --personal=./whitelist.en.pws | grep "^" && echo "No spelling errors"
