fdbdc4672d89adff5b1413ec54404d2517859fa4
Kyle Mitchell <kyle@kemitchell.com>
2015-12-08 14:04:32 -0800

    Fix spell checking test

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 set -e
 node validate.js
 node checkinputs.js
-cat *.cform | aspell --dont-backup list --personal=./whitelist.en.pws | grep "^"
+! cat *.cform | aspell --dont-backup list --personal=./whitelist.en.pws | grep "^"
