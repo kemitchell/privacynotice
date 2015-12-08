0dc14e2bb9e8f41851c6910cd7f49838b19010a0
Kyle Mitchell <kyle@kemitchell.com>
2015-12-08 15:05:03 -0800

    Print test commands

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-set -e
+set -e # Fail on error.
+set -v # Print commands to stderr as executed.
 node test/validate-inputs.js
 node test/compare-blanks-and-inputs.js
 ! cat *.cform | aspell --dont-backup list --personal=./whitelist.en.pws | grep "^"
